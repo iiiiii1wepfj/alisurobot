2e766f5cbec0cc86b4088bbc345da1880947b4e3
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-08-17 10:13:59 +0300

    Update rules.py

--- eduu/plugins/rules.py
+++ eduu/plugins/rules.py
@@ -8,6 +8,7 @@ from eduu.config import prefix
 from eduu.database import groups
 from eduu.utils import button_parser, commands, require_admin
 from eduu.utils.localization import use_chat_lang
+from eduu.utils.bot_error_log import logging_errors
 
 
 async def get_rules(chat_id: int):
@@ -24,6 +25,7 @@ async def set_rules(chat_id: int, rules):
 @Client.on_message(filters.command("setrules", prefix) & filters.group)
 @require_admin(permissions=["can_change_info"])
 @use_chat_lang()
+@logging_errors
 async def settherules(c: Client, m: Message, strings):
     if len(m.text.split()) > 1:
         await set_rules(m.chat.id, m.text.split(None, 1)[1])
@@ -35,6 +37,7 @@ async def settherules(c: Client, m: Message, strings):
 @Client.on_message(filters.command("resetrules", prefix) & filters.group)
 @require_admin(permissions=["can_change_info"])
 @use_chat_lang()
+@logging_errors
 async def delete_rules(c: Client, m: Message, strings):
     await set_rules(m.chat.id, None)
     await m.reply_text(strings("rules_deleted"))
@@ -42,6 +45,7 @@ async def delete_rules(c: Client, m: Message, strings):
 
 @Client.on_message(filters.command("rules", prefix) & filters.group)
 @use_chat_lang()
+@logging_errors
 async def show_rules(c: Client, m: Message, strings):
     gettherules = await get_rules(m.chat.id)
     rulestxt, rules_buttons = button_parser(gettherules)
@@ -58,6 +62,7 @@ async def show_rules(c: Client, m: Message, strings):
 
 @Client.on_message(filters.regex("^/start rules_") & filters.private)
 @use_chat_lang()
+@logging_errors
 async def show_rules_pvt(c: Client, m: Message, strings):
     cid_one = m.text.split("_")[1]
     gettherules = await get_rules(cid_one if cid_one.startswith("-") else f"-{cid_one}")
