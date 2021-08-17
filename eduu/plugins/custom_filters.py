267ccf48117c4db201190c788e8bf906e7628b45
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-08-17 10:06:29 +0300

    Update custom_filters.py

--- eduu/plugins/custom_filters.py
+++ eduu/plugins/custom_filters.py
@@ -11,6 +11,8 @@ from eduu.config import prefix
 from eduu.database import filters as filtersdb
 from eduu.utils import button_parser, commands, require_admin, split_quotes
 from eduu.utils.localization import use_chat_lang
+from eduu.utils.bot_error_log import logging_errors
+
 
 
 async def add_filter(
@@ -61,6 +63,7 @@ async def check_for_filters(chat_id: int, trigger):
 @Client.on_message(filters.command(["filter", "savefilter"], prefix))
 @require_admin(allow_in_private=True)
 @use_chat_lang()
+@logging_errors
 async def save_filter(c: Client, m: Message, strings):
     args = m.text.markdown.split(maxsplit=1)
     split_text = split_quotes(args[1])
@@ -145,6 +148,7 @@ async def save_filter(c: Client, m: Message, strings):
 @Client.on_message(filters.command(["delfilter", "rmfilter", "stop"], prefix))
 @require_admin(allow_in_private=True)
 @use_chat_lang()
+@logging_errors
 async def delete_filter(c: Client, m: Message, strings):
     args = m.text.markdown.split(maxsplit=1)
     trigger = args[1].lower()
@@ -163,6 +167,7 @@ async def delete_filter(c: Client, m: Message, strings):
 
 @Client.on_message(filters.command("filters", prefix))
 @use_chat_lang()
+@logging_errors
 async def get_all_filter(c: Client, m: Message, strings):
     chat_id = m.chat.id
     reply_text = strings("filters_list")
@@ -180,6 +185,7 @@ async def get_all_filter(c: Client, m: Message, strings):
 @Client.on_message(
     (filters.group | filters.private) & filters.text & filters.incoming, group=1
 )
+@logging_errors
 async def serve_filter(c: Client, m: Message):
     chat_id = m.chat.id
     text = m.text
