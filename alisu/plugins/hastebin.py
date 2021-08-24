bf511a80866ee3d62b270174306ebbc99b5f8758
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-08-24 09:59:03 +0300

    Update hastebin.py

--- alisu/plugins/hastebin.py
+++ alisu/plugins/hastebin.py
@@ -21,7 +21,7 @@ async def hastebin(c: Client, m: Message, strings):
         if m.reply_to_message.text:
             mean = m.reply_to_message.text
 
-        url = "https://hastebin.com/documents"
+        url: str = "https://hastebin.com/documents"
         r = await http.post(url, data=mean.encode("UTF-8"))
         url = f"https://hastebin.com/{r.json()['key']}"
         await m.reply_text(url, disable_web_page_preview=True)
