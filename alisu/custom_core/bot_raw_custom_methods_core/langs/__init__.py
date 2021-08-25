8238814602028e312eca9a40e8fb3bd8ec85404c
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-08-25 09:53:27 +0300

    Update __init__.py

--- alisu/custom_core/bot_raw_custom_methods_core/langs/__init__.py
+++ alisu/custom_core/bot_raw_custom_methods_core/langs/__init__.py
@@ -1,7 +1,9 @@
 from typing import List
 from .get_bot_locale_string_simple import get_bot_locale_string_simple
+from .get_bot_locale_langs_simple import get_bot_locale_langs_simple
 
 
 __all__: List[str] = [
     "get_bot_locale_string_simple",
+    "get_bot_locale_langs_simple",
 ]
