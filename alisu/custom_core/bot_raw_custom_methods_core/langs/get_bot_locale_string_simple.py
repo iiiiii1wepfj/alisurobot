5878bcf82f0b988235d29191a3232390f8681d10
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-08-25 11:17:18 +0300

    Update get_bot_locale_string_simple.py

--- alisu/custom_core/bot_raw_custom_methods_core/langs/get_bot_locale_string_simple.py
+++ alisu/custom_core/bot_raw_custom_methods_core/langs/get_bot_locale_string_simple.py
@@ -1,11 +1,12 @@
 from alisu.utils.localization import get_locale_string, langdict, default_language
 from functools import partial
+from typing import Optional
 
 
 def get_bot_locale_string_simple(
     category: str,
     string_name: str,
-    language: str = default_language,
+    language: Optional[str] = default_language,
 ):
     strings = partial(
         get_locale_string,
