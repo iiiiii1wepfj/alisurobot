e86cb83cddd079d2177831f739eefe94e0e3c258
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-08-30 12:12:43 +0300

    Update get_admin_permissions_raw.py

--- alisu/custom_core/custom_methods/get_admin_permissions_raw.py
+++ alisu/custom_core/custom_methods/get_admin_permissions_raw.py
@@ -1,22 +1,22 @@
 from typing import Union
-from pyrogram import Client
 from pyrogram.raw.functions.channels import GetParticipant
 
 
-async def get_admin_permissions_raw(
-    client: Client,
-    chat_id: Union[int, str],
-    user_id: Union[int, str],
-):
-    chat = await client.resolve_peer(chat_id)
-    user = await client.resolve_peer(user_id)
+class GetAdminPermissionsRaw:
+    async def get_admin_permissions_raw(
+        self,
+        chat_id: Union[int, str],
+        user_id: Union[int, str],
+    ):
+        chat = await self.resolve_peer(chat_id)
+        user = await self.resolve_peer(user_id)
 
-    r = (
-        await client.send(
-            GetParticipant(
-                channel=chat,
-                user_id=user,
+        r = (
+            await self.send(
+                GetParticipant(
+                    channel=chat,
+                    user_id=user,
+                )
             )
-        )
-    ).participant.admin_rights
-    return r
+        ).participant.admin_rights
+        return r
