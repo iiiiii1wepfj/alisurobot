8cff1a2dbed376ce67ad67bc0521739f36e3da81
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-08-30 14:09:10 +0300

    Update __init__.py

--- alisu/custom_core/custom_methods/__init__.py
+++ alisu/custom_core/custom_methods/__init__.py
@@ -1,5 +1,6 @@
 from typing import List
 from .get_admin_permissions_raw import GetAdminPermissionsRaw
+from .get_chat_member_raw import GetChatMemberRaw
 
 
 class custom_methods(GetAdminPermissionsRaw):
