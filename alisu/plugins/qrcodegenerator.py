634476f970d25dc984c2f64a44abfef02e9446dc
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-02 14:07:40 +0300

    Update qrcodegenerator.py

--- alisu/plugins/qrcodegenerator.py
+++ alisu/plugins/qrcodegenerator.py
@@ -11,14 +11,20 @@ import qrcode
 
 @Client.on_message(filters.command("gen_qr", prefix))
 async def genqrcodecmd(c: Client, m: Message):
-    if m.reply_to_message.caption:
-        qrcodetext = m.reply_to_message.caption
-    elif m.reply_to_message.text:
-        qrcodetext = m.reply_to_message.text
-    elif len(m.text.split()) > 1:
-        qrcodetext = m.text.split(None, 1)[1]
+    if m.reply_to_message:
+        if m.reply_to_message.caption:
+            qrcodetext = m.reply_to_message.caption
+        elif m.reply_to_message.text:
+            qrcodetext = m.reply_to_message.text
+        elif len(m.text.split()) > 1:
+            qrcodetext = m.text.split(None, 1)[1]
+        else:
+            return
     else:
-        return
+        if len(m.text.split()) > 1:
+            qrcodetext = m.text.split(None, 1)[1]
+        else:
+            return
     make_qr_code = qrcode.make(qrcodetext)
     bytes_qr_code = BytesIO()
     make_qr_code.save(bytes_qr_code)
