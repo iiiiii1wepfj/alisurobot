7af7cf9eb3b37b728117acfe9fd3ad7fbad031a1
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-06 13:16:59 +0300

    Update dice.py

--- alisu/plugins/dice.py
+++ alisu/plugins/dice.py
@@ -7,13 +7,27 @@ from alisu.utils.localization import use_chat_lang
 from alisu.utils.bot_error_log import logging_errors
 
 
-@Client.on_message(filters.command(["dice", "dados"], prefix))
+@Client.on_message(
+    filters.command(
+        [
+            "dice",
+            "dados",
+        ],
+        prefix,
+    )
+)
 @use_chat_lang()
 @logging_errors
 async def dice(c: Client, m: Message, strings):
-    dicen = await c.send_dice(m.chat.id, reply_to_message_id=m.message_id)
+    dicen = await c.send_dice(
+        m.chat.id,
+        reply_to_message_id=m.message_id,
+    )
     await dicen.reply_text(
-        strings("result").format(number=dicen.dice.value), quote=True
+        strings("result").format(
+            number=dicen.dice.value,
+        ),
+        quote=True,
     )
 
 
