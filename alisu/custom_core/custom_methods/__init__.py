723c79028c4351169af1279fa6ec82d33817f3c8
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-10 11:43:28 +0300

    Update __init__.py

--- alisu/custom_core/custom_methods/__init__.py
+++ alisu/custom_core/custom_methods/__init__.py
@@ -2,11 +2,13 @@ from typing import List
 from .get_admin_permissions_raw import GetAdminPermissionsRaw
 from .get_chat_member_raw import GetChatMemberRaw
 from .get_raw_message_from_pyrogram import GetRawMessageFromPyrogram
+from .get_raw_message_custom import GetRawMessageCustom
 
 
 class custom_methods(
     GetAdminPermissionsRaw,
     GetChatMemberRaw,
     GetRawMessageFromPyrogram,
+    GetRawMessageCustom,
 ):
     pass
