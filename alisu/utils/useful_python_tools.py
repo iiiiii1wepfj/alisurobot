719f3a58308fa588f1f65b02dd079bd4531a8805
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-12 17:45:56 +0300

    Create useful_python_tools.py

---
+++ alisu/utils/useful_python_tools.py
@@ -0,0 +1,10 @@
+def dict_snake_case(data: dict, same_for_sub_dict: bool = True):
+    output_dict = {}
+    for data_keys, data_values in data.items():
+        if same_for_sub_dict:
+            if isinstance(data_values, dict):
+                data_values = dict_snake_case(data_values)
+            output_dict[data_keys.replace(" ", "_")] = data_values
+        else:
+            output_dict[data_keys.replace(" ", "_")] = data_values
+    return output_dict
