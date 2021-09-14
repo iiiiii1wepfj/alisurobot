eb96c5a90c2a2ea7366f9812c7ee347dd053b716
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-14 10:17:56 +0300

    Update useful_python_tools.py

--- alisu/utils/useful_python_tools.py
+++ alisu/utils/useful_python_tools.py
@@ -1,4 +1,12 @@
+from http.client import (
+    responses as http_status_codes_dict,
+)
 from collections import namedtuple
+import json
+
+
+class HttpStatusCodeNotFound(Exception):
+    pass
 
 
 def dict_snake_case(data: dict, same_for_sub_dict: bool = True):
@@ -15,3 +23,14 @@ def dict_snake_case(data: dict, same_for_sub_dict: bool = True):
 
 def dict2py(data: dict, name: str = "pyobject"):
     return namedtuple(name, data.keys())(*data.values())
+
+
+def json_prettify(data: dict):
+    return json.dumps(data, indent=4)
+
+
+def http_status_code_info(code: int):
+    if code not in http_status_codes_dict:
+        raise HttpStatusCodeNotFound(f"the http status code {code} is not found.")
+    else:
+        return http_status_codes_dict[code]
