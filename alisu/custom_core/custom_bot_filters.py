13aab4b2ad7c1b3ac1756aa56ab5d2997cca9a95
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-14 09:39:41 +0300

    Create custom_bot_filters.py

---
+++ alisu/custom_core/custom_bot_filters.py
@@ -0,0 +1,27 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from typing import Union, Pattern
+import re
+
+viewsfilter = filters.create(lambda _, __, Message: Message.views)
+
+non_anonymous_poll = filters.create(
+    lambda *_: _[2].poll is not None and not _[2].poll.is_anonymous
+)
+
+
+def biofilter(pattern: Union[str, Pattern], flags: int = 0):
+    async def func(flt, client, message):
+        if not message.from_user:
+            return False
+        _ = await client.get_chat(message.from_user.id)
+        value = _.bio
+        if value:
+            Update.matches = list(flt.p.finditer(value)) or None
+            return bool(Update.matches)
+
+    return filters.create(
+        func,
+        "biofilter",
+        p=pattern if isinstance(pattern, Pattern) else re.compile(pattern, flags),
+    )
