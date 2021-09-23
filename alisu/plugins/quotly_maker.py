88d7a16eca763f082b7a11ef3ae5e5cff4d12512
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-23 19:44:34 +0300

    Create quotly_maker.py

---
+++ alisu/plugins/quotly_maker.py
@@ -0,0 +1,151 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from alisu.config import prefix
+from alisu.utils.consts import http
+from alisu.utils.bot_error_log import logging_errors
+
+from io import BytesIO
+
+
+class QuotlyException(Exception):
+    pass
+
+
+# based on https://github.com/TheHamkerCat/Python_ARQ/blob/master/Python_ARQ/arq.py
+async def pyrogram_to_quotly(messages):
+    if not isinstance(messages, list):
+        messages = [messages]
+
+    payload = {
+        "type": "quote",
+        "format": "png",
+        "backgroundColor": "#1b1429",
+        "messages": [
+            {
+                "entities": [
+                    {
+                        "type": entity.type,
+                        "offset": entity.offset,
+                        "length": entity.length,
+                    }
+                    for entity in message.entities
+                ]
+                if message.entities
+                else [],
+                "chatId": message.forward_from.id
+                if message.forward_from
+                else message.from_user.id,
+                "avatar": True,
+                "from": {
+                    "id": message.from_user.id,
+                    "username": message.from_user.username
+                    if message.from_user.username
+                    else "",
+                    "photo": {
+                        "small_file_id": message.from_user.photo.small_file_id,
+                        "small_photo_unique_id": message.from_user.photo.small_photo_unique_id,
+                        "big_file_id": message.from_user.photo.big_file_id,
+                        "big_photo_unique_id": message.from_user.photo.big_photo_unique_id,
+                    }
+                    if message.from_user.photo
+                    else "",
+                    "type": message.chat.type,
+                    "name": f"{message.from_user.first_name} {message.from_user.last_name or ''}".rstrip(),
+                }
+                if not message.forward_from
+                else {
+                    "id": message.forward_from.id,
+                    "username": message.forward_from.username
+                    if message.forward_from.username
+                    else "",
+                    "photo": {
+                        "small_file_id": message.forward_from.photo.small_file_id,
+                        "small_photo_unique_id": message.forward_from.photo.small_photo_unique_id,
+                        "big_file_id": message.forward_from.photo.big_file_id,
+                        "big_photo_unique_id": message.forward_from.photo.big_photo_unique_id,
+                    }
+                    if message.forward_from.photo
+                    else "",
+                    "type": message.chat.type,
+                    "name": f"{message.forward_from.first_name} {message.forward_from.last_name or ''}".rstrip(),
+                },
+                "text": message.text if message.text else "",
+                "replyMessage": (
+                    {
+                        "name": f"{message.reply_to_message.from_user.first_name} {message.reply_to_message.from_user.last_name or ''}".rstrip(),
+                        "text": message.reply_to_message.text,
+                        "chatId": message.reply_to_message.from_user.id,
+                    }
+                    if message.reply_to_message
+                    else {}
+                )
+                if len(messages) == 1
+                else {},
+            }
+            for message in messages
+        ],
+    }
+    r = await http.post(f"https://bot.lyo.su/quote/generate.png", json=payload)
+    if not r.is_error:
+        return r.read()
+    else:
+        raise QuotlyException(r.json())
+
+
+def isArgInt(txt) -> list:
+    count = txt
+    try:
+        count = int(count)
+        return [True, count]
+    except ValueError:
+        return [False, 0]
+
+
+@Client.on_message(filters.command("q", prefix))
+@logging_errors
+async def msg_quotly_cmd(c: Client, m: Message):
+    if len(m.text.split()) > 1:
+        check_arg = isArgInt(m.command[1])
+        if check_arg:
+            if check_arg[1] < 2 or check_arg[1] > 10:
+                await m.reply_text("the arg range must to be between 2 to 10")
+            else:
+                try:
+                    messages = [
+                        i
+                        for i in await c.get_messages(
+                            chat_id=m.chat.id,
+                            message_ids=range(
+                                m.reply_to_message.message_id,
+                                m.reply_to_message.message_id + (count + 5),
+                            ),
+                            replies=-1,
+                        )
+                        if not i.empty and not i.media
+                    ]
+                except:
+                    return await m.reply_text("¯\\_(ツ)_/¯")
+                try:
+                    make_quotly = await pyrogram_to_quotly(messages)
+                    bio_sticker = BytesIO(make_quotly)
+                    bio_sticker.name = "biosticker.webp"
+                    return await m.reply_sticker(bio_sticker)
+                except:
+                    return await m.reply_text("¯\\_(ツ)_/¯")
+        else:
+            pass
+    try:
+        messages_one = await c.get_messages(
+            chat_id=m.chat.id, message_ids=m.reply_to_message.message_id, replies=-1
+        )
+        messages = [messages_one]
+    except:
+        return await m.reply_text("¯\\_(ツ)_/¯")
+    try:
+        make_quotly = await pyrogram_to_quotly(messages)
+        bio_sticker = BytesIO(make_quotly)
+        bio_sticker.name = "biosticker.webp"
+        return await m.reply_sticker(bio_sticker)
+    except:
+        return await m.reply_text("¯\\_(ツ)_/¯")
