340c8fe71bde27e8ca2678159e64398f72bf710d
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-23 20:31:57 +0300

    Update quotly_maker.py

--- alisu/plugins/quotly_maker.py
+++ alisu/plugins/quotly_maker.py
@@ -137,17 +137,11 @@ async def msg_quotly_cmd(c: Client, m: Message, strings):
                     return await m.reply_text("¯\\_(ツ)_/¯")
         else:
             pass
-    try:
-        messages_one = await c.get_messages(
-            chat_id=m.chat.id, message_ids=m.reply_to_message.message_id, replies=-1
-        )
-        messages = [messages_one]
-    except:
-        return await m.reply_text("¯\\_(ツ)_/¯")
-    try:
-        make_quotly = await pyrogram_to_quotly(messages)
-        bio_sticker = BytesIO(make_quotly)
-        bio_sticker.name = "biosticker.webp"
-        return await m.reply_sticker(bio_sticker)
-    except:
-        return await m.reply_text("¯\\_(ツ)_/¯")
+    messages_one = await c.get_messages(
+        chat_id=m.chat.id, message_ids=m.reply_to_message.message_id, replies=-1
+    )
+    messages = [messages_one]
+    make_quotly = await pyrogram_to_quotly(messages)
+    bio_sticker = BytesIO(make_quotly)
+    bio_sticker.name = "biosticker.webp"
+    return await m.reply_sticker(bio_sticker)
