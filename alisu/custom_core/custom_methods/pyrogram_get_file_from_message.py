8c25d4c78952baeea2a73f8896a9ffc8045bdbbe
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-25 13:53:19 +0300

    Update pyrogram_get_file_from_message.py

--- alisu/custom_core/custom_methods/pyrogram_get_file_from_message.py
+++ alisu/custom_core/custom_methods/pyrogram_get_file_from_message.py
@@ -2,18 +2,33 @@ from pyrogram.types import Message
 from pyrogram.file_id import FileId
 from pyrogram.scaffold import Scaffold
 
+
 class PyrogramGetFileFromMessage(Scaffold):
- async def pyrogram_get_file_from_message(self, message: Message):
-  media_list_support = ("audio", "document", "photo", "sticker", "animation", "video", "voice", "video_note", "new_chat_photo")
-  if isinstance(msg, Message):
-   for kind in media_list_support:
-    mediatype = getattr(msg, kind, None)
-    if mediatype is not None:
-     break
-   else:
-    raise ValueError("can't found the media type")
-  else:
-   mediatype = msg
-  pyro_file_id_obj = FileId.decode(mediatype.file_id)
-  r = await self.get_file(file_id=pyro_file_id_obj, file_size=getattr(mediatype, "file_size", 0), progress=None)
-  return r
+    async def pyrogram_get_file_from_message(self, message: Message):
+        media_list_support = (
+            "audio",
+            "document",
+            "photo",
+            "sticker",
+            "animation",
+            "video",
+            "voice",
+            "video_note",
+            "new_chat_photo",
+        )
+        if isinstance(message, Message):
+            for kind in media_list_support:
+                mediatype = getattr(message, kind, None)
+                if mediatype is not None:
+                    break
+            else:
+                raise ValueError("can't found the media type")
+        else:
+            mediatype = message
+        pyro_file_id_obj = FileId.decode(mediatype.file_id)
+        r = await self.get_file(
+            file_id=pyro_file_id_obj,
+            file_size=getattr(mediatype, "file_size", 0),
+            progress=None,
+        )
+        return r
