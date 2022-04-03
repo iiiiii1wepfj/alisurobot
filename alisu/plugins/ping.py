75bc1a9dd49b12112d5ec813bcc5d23ca0b801b7
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2022-04-03 15:30:13 +0300

    Update ping.py

--- alisu/plugins/ping.py
+++ alisu/plugins/ping.py
@@ -8,7 +8,7 @@ from alisu.utils import commands
 from alisu.utils.bot_error_log import logging_errors
 
 
-@Client.on_message(filters.command("ping", prefix))
+@Client.on_message(filters.command("ping", prefix) & ~filters.edited)
 @logging_errors
 async def ping(c: Client, m: Message):
     first = datetime.now()
