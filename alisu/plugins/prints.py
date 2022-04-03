458459a33dca91fb0ad3f87a02d66e9942dbd165
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2022-04-03 15:30:49 +0300

    Update prints.py

--- alisu/plugins/prints.py
+++ alisu/plugins/prints.py
@@ -10,7 +10,7 @@ from alisu.utils.localization import use_chat_lang
 from alisu.utils.bot_error_log import logging_errors
 
 
-@Client.on_message(filters.command("print", prefix))
+@Client.on_message(filters.command("print", prefix) & ~filters.edited)
 @use_chat_lang()
 @logging_errors
 async def prints(c: Client, m: Message, strings):
