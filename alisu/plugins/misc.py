6eac3209f4bf51a4f4398e3f847742f72f03cd8b
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2022-04-24 17:06:50 +0300

    Update misc.py

--- alisu/plugins/misc.py
+++ alisu/plugins/misc.py
@@ -14,7 +14,7 @@ from alisu.utils.bot_error_log import logging_errors
 from alisu.utils.passindexerr import pass_index_error
 
 
-@Client.on_message(filters.command("mark", prefix) & ~filters.edited)
+@Client.on_message(filters.command("mark", prefix))
 @use_chat_lang()
 @logging_errors
 async def mark(c: Client, m: Message, strings):
@@ -29,7 +29,7 @@ async def mark(c: Client, m: Message, strings):
     )
 
 
-@Client.on_message(filters.command("html", prefix) & ~filters.edited)
+@Client.on_message(filters.command("html", prefix))
 @use_chat_lang()
 @logging_errors
 async def html(c: Client, m: Message, strings):
@@ -44,7 +44,7 @@ async def html(c: Client, m: Message, strings):
     )
 
 
-@Client.on_message(filters.command("admins", prefix) & filters.group & ~filters.edited)
+@Client.on_message(filters.command("admins", prefix) & filters.group)
 @use_chat_lang()
 @logging_errors
 async def mentionadmins(c: Client, m: Message, strings):
@@ -62,7 +62,6 @@ async def mentionadmins(c: Client, m: Message, strings):
     (filters.command("report", prefix) | filters.regex("^@admin"))
     & filters.group
     & filters.reply
-    & ~filters.edited
 )
 @use_chat_lang()
 @logging_errors
@@ -84,7 +83,7 @@ async def reportadmins(c: Client, m: Message, strings):
             )
 
 
-@Client.on_message(filters.command("token") & ~filters.edited)
+@Client.on_message(filters.command("token"))
 @use_chat_lang()
 async def getbotinfo(c: Client, m: Message, strings):
     if len(m.command) == 1:
@@ -112,7 +111,6 @@ async def getbotinfo(c: Client, m: Message, strings):
     & filters.group
     & filters.regex(r"(?i)^rt$")
     & filters.text
-    & ~filters.edited
 )
 @logging_errors
 async def rtcommand(c: Client, m: Message):
@@ -148,21 +146,21 @@ async def rtcommand(c: Client, m: Message):
         )
 
 
-@Client.on_message(filters.command("urlencode", prefix) & ~filters.edited)
+@Client.on_message(filters.command("urlencode", prefix))
 @logging_errors
 @pass_index_error
 async def urlencodecmd(c: Client, m: Message):
     await m.reply_text(quote(m.text.split(None, 1)[1]))
 
 
-@Client.on_message(filters.command("urldecode", prefix) & ~filters.edited)
+@Client.on_message(filters.command("urldecode", prefix))
 @logging_errors
 @pass_index_error
 async def urldecodecmd(c: Client, m: Message):
     await m.reply_text(unquote(m.text.split(None, 1)[1]))
 
 
-@Client.on_message(filters.command("bug", prefix) & ~filters.edited)
+@Client.on_message(filters.command("bug", prefix))
 @use_chat_lang()
 @logging_errors
 async def bug_report_cmd(c: Client, m: Message, strings):
@@ -189,7 +187,7 @@ async def bug_report_cmd(c: Client, m: Message, strings):
         await m.reply(strings("err_no_bug_to_report"))
 
 
-@Client.on_message(filters.command("request", prefix) & ~filters.edited)
+@Client.on_message(filters.command("request", prefix))
 @logging_errors
 async def request_cmd(c: Client, m: Message):
     if len(m.text.split()) > 1:
@@ -215,7 +213,7 @@ async def request_cmd(c: Client, m: Message):
         )
 
 
-@Client.on_message(filters.command("parsebutton") & ~filters.edited)
+@Client.on_message(filters.command("parsebutton"))
 @use_chat_lang()
 @logging_errors
 async def button_parse_helper(c: Client, m: Message, strings):
@@ -227,7 +225,7 @@ async def button_parse_helper(c: Client, m: Message, strings):
         await m.reply_text(strings("parsebtn_err"))
 
 
-@Client.on_message(filters.command("donate") & ~filters.edited)
+@Client.on_message(filters.command("donate"))
 @logging_errors
 async def donate_to_owner_cmd(c: Client, m: Message):
     await m.reply_text("https://paypal.me/itayki")
