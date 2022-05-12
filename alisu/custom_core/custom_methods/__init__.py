6a4c91318ee63b3857e0bc3dbaf77c25944f8582
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2022-05-12 15:48:23 +0300

    Update __init__.py

--- alisu/custom_core/custom_methods/__init__.py
+++ alisu/custom_core/custom_methods/__init__.py
@@ -3,7 +3,6 @@ from .get_admin_permissions_raw import GetAdminPermissionsRaw
 from .get_chat_member_raw import GetChatMemberRaw
 from .get_raw_message_from_pyrogram import GetRawMessageFromPyrogram
 from .get_raw_message_custom import GetRawMessageCustom
-from .pyrogram_get_file_from_message import PyrogramGetFileFromMessage
 
 
 class custom_methods(
@@ -11,6 +10,5 @@ class custom_methods(
     GetChatMemberRaw,
     GetRawMessageFromPyrogram,
     GetRawMessageCustom,
-    PyrogramGetFileFromMessage,
 ):
     pass
